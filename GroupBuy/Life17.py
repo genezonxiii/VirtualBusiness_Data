71fb64d60b9577db8ba6ca30ccf664bdce02cc76
robinkuo168 <robin.kuo168@gmail.com>
2017-01-04 15:45:39 +0800

    Add class chinatime , crazymike ,HerBuy,Ibon,Life17,mybenefit,pcone,popular,sale123

---
+++ GroupBuy/Life17.py
@@ -0,0 +1,45 @@
+# -*-  coding: utf-8  -*-
+# __author__ = '10408001'
+
+import logging
+import xlrd
+from GroupBuy.buy123 import buy123
+#17P團購
+class Life17(buy123):
+    #拆解字串中的包數
+    def parserString(self,SourceString,keyWord):
+        return SourceString.find(keyWord)
+
+    # 解析原始檔
+    def parserFile(self, GroupID, UserID, LogisticsID=2, ProductCode=None, inputFile=None, outputFile=None):
+        self.init_log('17P_Data', GroupID, UserID, ProductCode, inputFile)
+        try:
+            data = xlrd.open_workbook(inputFile)
+            table = data.sheets()[0]
+            result = []
+            # 讀 excel 檔
+            for row_index in range(1, table.nrows):
+                tmp = []
+                tmp.append(table.cell(row_index, 1).value) #訂單編號
+                tmp.append(table.cell(row_index, 3).value)  # 收件人
+                tmp.append(table.cell(row_index, 5).value)  # 收件地址
+                tmp.append('0' + self.ReplaceField(str(table.cell(row_index, 4).value), '.'))  # 電話
+                tmp.append(table.cell(row_index, 6).value)  # 方案名稱
+                #暫時以 "菌" 及"包" 來判斷,需要跟悠活原力確認
+                word = self.ReplaceField(table.cell(row_index, 6).value,u"包")
+                tmp.append(int(word[word.find(u"菌")+1:])/30) # 訂購方案
+                tmp.append(table.cell(row_index, 9).value)  # 訂單份數
+                tmp.append("")  # 訂購人
+                result.append(tmp)
+            # self.writeXls(LogisticsID, result, outputFile)
+        except Exception as e:
+            logging.error(e.message)
+            return 'failure'
+
+if __name__ == '__main__':
+    buy = Life17()
+    # word = buy.ReplaceField("[24H出貨]欣敏立清-草莓多多益生菌120包+贈德國Purafit-維他命C發泡錠Vitamin C","包")
+    # print int(word[word.find("菌")+3:])/30
+    buy.parserFile('robintest', 'test',2, 'DS',
+                  inputFile=u'C:/Users/10408001/Desktop/團購平台訂單資訊/17P\原始檔/2016.12.26/10397319_[24H出貨]欣敏立清-草莓多多益生菌_出貨清冊 (1).xls', \
+                  outputFile=u'C:/Users/10408001/Desktop/20161229-17P出貨單.xls')
---
+++ GroupBuy/popular.py
@@ -0,0 +1,54 @@
+# -*-  coding: utf-8  -*-
+# __author__ = '10408001'
+import logging
+import codecs
+from GroupBuy.buy123 import buy123
+# 小P團購
+class popular(buy123):
+    # 解析原始檔
+    def parserFile(self, GroupID, UserID, LogisticsID=2, ProductCode=None, inputFile=None, outputFile=None):
+        self.init_log('Popular_Data', GroupID, UserID, ProductCode, inputFile)
+        try:
+            data = codecs.open(inputFile,'rb',encoding='big5')
+            tmpdata,result = [],[]
+            # 讀 csv 檔
+            i = 0
+            for row in data:
+                if i > 0 :
+                    rowResult = row.split(',')
+                    tmp = []
+                    for j in rowResult:
+                        if rowResult[0]=="" or rowResult[0]==" ":
+                            break
+                        j = j.replace("'","")
+                        tmp.append(j)
+                    if tmp <> []:
+                        tmpdata.append(tmp)
+                i +=1
+
+            for row in tmpdata:
+                tmp = []
+                tmp.append(row[0])  # 訂單編號
+                tmp.append(row[12])  # 收件人
+                tmp.append(row[13])  # 收件地址
+                tmp.append(row[14])  # 電話
+                tmp.append(row[3])  # 檔次名稱
+                tmp.append(1) #盒數
+                # word = self.ReplaceField(rowResult[17], u'盒')
+                # print self.getResultForDigit(word)
+                # tmp.append(self.getResultForDigit(word[word.find(u"多多")+2])) # 商品名稱
+                tmp.append(row[8])   #訂購總數
+                tmp.append(row[11])  # 訂購人
+                result.append(tmp)
+
+            self.writeXls(LogisticsID, result, outputFile)
+        except Exception as e:
+            logging.error(e.message)
+            return 'failure'
+
+
+if __name__ == '__main__':
+    buy = popular()
+    buy.parserFile('robintest', 'test', 2 , 'MS',
+                  inputFile=u'C:/Users/10408001/Desktop/團購平台訂單資訊/小P/原始檔/2016.10.28/20161028.csv', \
+                  outputFile=u'C:/Users/10408001/Desktop/20170104-popular出貨單.xls')
