58bd2597e89e154205776f41a0bf9a2b35110744
robinkuo168 <robin.kuo168@gmail.com>
2017-01-23 17:01:43 +0800

    modify class buy123 function getResultForDigit

--- GroupBuy/buy123.py
+++ GroupBuy/buy123.py
@@ -79,13 +79,11 @@ class buy123():
 
     # 中文數字轉換成阿拉伯數字
     def getResultForDigit(self, a, encoding="utf-8"):
-        Numdict = {u'零': 0, u'一': 1, u'二': 2, u'三': 3, u'四': 4, u'五': 5, u'六': 6, u'七': 7, u'八': 8, u'九': 9,
-                   u'十': 10, \
-                   u'百': 100, u'千': 1000, u'万': 10000, u'０': 0, u'１': 1, u'２': 2, u'３': 3, u'４': 4, u'５': 5,
-                   u'６': 6, \
-                   u'７': 7, u'８': 8, u'９': 9, u'壹': 1, u'貳': 2, u'參': 3, u'肆': 4, u'伍': 5, u'陸': 6, u'柒': 7,
-                   u'捌': 8, \
-                   u'玖': 9, u'拾': 10, u'佰': 100, u'仟': 1000, u'萬': 10000, u'億': 100000000}
+        Numdict = {u'零': 0, u'一': 1, u'二': 2, u'三': 3, u'四': 4, u'五': 5, u'六': 6, u'七': 7, u'八': 8, u'九': 9,\
+                   u'十': 10, u'0': 0, u'1': 1, u'2': 2, u'3': 3, u'4': 4, u'5': 5, u'6': 6, u'7': 7, u'8': 8, u'9': 9,\
+                   u'10': 10, u'百': 100, u'千': 1000, u'万': 10000, u'０': 0, u'１': 1, u'２': 2, u'３': 3, u'４': 4,\
+                   u'５': 5, u'６': 6,  u'７': 7, u'８': 8, u'９': 9, u'壹': 1, u'貳': 2, u'參': 3, u'肆': 4, u'伍': 5,\
+                   u'陸': 6, u'柒': 7, u'捌': 8, u'玖': 9, u'拾': 10, u'佰': 100, u'仟': 1000, u'萬': 10000, u'億': 100000000}
         if isinstance(a, str):
             a = a.decode(encoding)
         count = 0
@@ -147,6 +145,7 @@ class buy123():
         self.init_log('Buy123_Data',GroupID, UserID , ProductCode , inputFile)
         success = False
         try:
+            self.getRegularEx(GroupID)
             data = xlrd.open_workbook(inputFile)
             table = data.sheets()[0]
             result = []
@@ -159,10 +158,11 @@ class buy123():
                 tmp.append(table.cell(row_index, 2).value)                                      #收件地址
                 tmp.append('0' + self.ReplaceField(str(table.cell(row_index, 3).value),'.'))      #電話
                 tmp.append(table.cell(row_index, 5).value)  #檔次名稱
-                order = self.ReplaceField(table.cell(row_index, 6).value,u'盒')
-                if "." in order :
-                    order = order.split(".")[1].strip()
-                tmp.append(order)              #訂購方案
+                tmp.append(self.getResultForDigit(self.parserRegularEx(table.cell(row_index, 6).value))) #訂購方案
+                # order = self.ReplaceField(table.cell(row_index, 6).value,u'盒')
+                # if "." in order :
+                #     order = order.split(".")[1].strip()
+                # tmp.append(order)              #訂購方案
                 # tmp.append(self.ReplaceField(table.cell(row_index, 6).value,u'盒'))              #訂購方案
                 tmp.append(table.cell(row_index, 7).value)                                      #組數
                 tmp.append(self.ReplaceField(table.cell(row_index, 8).value,'/'))                #訂購人                                                   #訂購人
@@ -276,8 +276,7 @@ class buy123():
             self.RegularEX.append(value)
 
     # 使用 RegularExpress 解析"方案名稱"中的數量
-    def parserRegularEx(self,GroupID,value):
-        self.getRegularEx(GroupID)
+    def parserRegularEx(self,value):
         matchWord = None
         try:
             for row in self.RegularEX:
